14b9933d477906783eedb65e2607ae9988a3a83d
Foreu Entertainment <foreuentertainment@gmail.com>
2017-03-17 23:48:41 +0530

    Map completed

--- app/src/main/java/com/visdenz/calme/MainActivity.java
+++ app/src/main/java/com/visdenz/calme/MainActivity.java
@@ -1,77 +1,84 @@
 package com.visdenz.calme;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.speech.tts.TextToSpeech;
+import android.net.Uri;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+public class MainActivity extends AppCompatActivity {
 
-    private TextToSpeech tts;
-    private Button ttsButton, mapButton;
-    private EditText ttsInputField;
+    private static final int REQUEST_SPEECH_INPUT_CODE = 100;
+    private static final CharSequence GO_TO_MAP = "go to";
+    private TextView speechText;
+    private ImageButton speakButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tts = new TextToSpeech(this, this);
-        ttsButton = (Button) findViewById(R.id.ttsButton);
-        mapButton = (Button) findViewById(R.id.mapButton);
-        ttsInputField = (EditText) findViewById(R.id.ttsInputField);
+        speechText = (TextView) findViewById(R.id.speechText);
+        speakButton = (ImageButton) findViewById(R.id.speakButton);
 
-        ttsButton.setOnClickListener(new View.OnClickListener() {
+        speakButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                speakOut();
+                promptSpeechInput();
             }
         });
-
-        mapButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, MapsActivity.class);
-                startActivity(i);
-            }
-        });
-
     }
 
-    private void speakOut() {
-        String convertedText = ttsInputField.getText().toString();
-        tts.speak(convertedText, TextToSpeech.QUEUE_FLUSH, null);
+    private void promptSpeechInput() {
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        i.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(i, REQUEST_SPEECH_INPUT_CODE);
+        } catch (ActivityNotFoundException e) {
+            Log.e("exception", e.toString());
+        }
     }
 
     @Override
-    public void onInit(int status) {
-        if(status == TextToSpeech.SUCCESS) {
-            int result = tts.setLanguage(Locale.US);
-            if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("TTS", "Language not supported");
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_SPEECH_INPUT_CODE: {
+                if(resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String words = result.get(0);
+                    speechText.setText(result.get(0));
+                    if(words.contains(GO_TO_MAP)) {
+                        String extractedText = words.substring(words.indexOf("go to ") + 6);
+                        String[] splited = extractedText.split("\\s+");
+                        String address = "";
+                        for(int i=0; i<splited.length; i++) {
+                            address = address + splited[i] + "+";
+                        }
+                        Uri gmmIntentUri = Uri.parse("google.navigation:q=" + address);
+                        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                        mapIntent.setPackage("com.google.android.apps.maps");
+                        startActivity(mapIntent);
+                    }
+                }
             }
-            else {
-                ttsButton.setEnabled(true);
-                speakOut();
-            }
-        }
-        else {
-            Log.e("TTS", "TTS Initialization Failed");
         }
     }
 
     @Override
     protected void onDestroy() {
-        if(tts != null) {
-            tts.stop();
-            tts.shutdown();
-        }
         super.onDestroy();
     }
 }
