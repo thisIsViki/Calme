d93c96b532e50c770062ae7478818da4de779ea3
Foreu Entertainment <foreuentertainment@gmail.com>
2017-03-20 00:33:12 +0530

    apk completed

--- app/src/main/java/com/visdenz/calme/MainActivity.java
+++ app/src/main/java/com/visdenz/calme/MainActivity.java
@@ -11,7 +11,9 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.net.Uri;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
 import android.support.annotation.NonNull;
@@ -30,12 +32,14 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener, SensorEventListener {
 
     private static final int REQUEST_SPEECH_INPUT_CODE = 100;
     private static final int MY_PERMISSION_READ_SMS = 101;
     private static final int MY_PERMISSION_SEND_SMS = 102;
+    private static final int MY_PERMISSION_RECORD_AUDIO = 103;
     private static final CharSequence GO_TO_MAP = "go to";
     private static final CharSequence READ_SMS = "read SMS";
     private static final CharSequence SEND_MY_SMS = "send SMS";
@@ -44,7 +48,9 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     private String phoneNo, msgContent;
     private SensorManager sensorManager;
     private long lastUpdate;
-    private boolean isActivityVisible = true;
+    private SpeechRecognizer mSpeechRecognizer;
+    private Intent mSpeechRecognizerIntent;
+    private boolean isOpen = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,88 +63,87 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         lastUpdate = System.currentTimeMillis();
 
-        promptSpeechInput();
+        if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.RECORD_AUDIO)) {
+                Log.i("permission", "should show request permission rationale");
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, MY_PERMISSION_RECORD_AUDIO);
+            }
+        }
+        else {
+            promptSpeechInput();
+        }
+
     }
 
     private void promptSpeechInput() {
-        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        i.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.speech_prompt));
-        try {
-            startActivityForResult(i, REQUEST_SPEECH_INPUT_CODE);
-        } catch (ActivityNotFoundException e) {
-            Log.e("exception", e.toString());
-        }
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.speech_prompt));
+        SpeechRecognizerListener listener = new SpeechRecognizerListener();
+        mSpeechRecognizer.setRecognitionListener(listener);
+        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
-            case REQUEST_SPEECH_INPUT_CODE: {
-                if(resultCode == RESULT_OK && data != null) {
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    String words = result.get(0);
-                    speechText.setText(result.get(0));
-                    if(words.contains(GO_TO_MAP)) {
-                        String extractedText = words.substring(words.indexOf("go to ") + 6);
-                        String[] splited = extractedText.split("\\s+");
-                        String address = "";
-                        for(int i=0; i<splited.length; i++) {
-                            address += splited[i] + "+";
-                        }
-                        Uri gmmIntentUri = Uri.parse("google.navigation:q=" + address);
-                        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                        mapIntent.setPackage("com.google.android.apps.maps");
-                        startActivity(mapIntent);
-                    }
-                    else if(words.contains(READ_SMS)) {
-                        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
-                            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_SMS)) {
-                                Log.i("permission", "should show request permission rationale");
-                            } else {
-                                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS}, MY_PERMISSION_READ_SMS);
-                            }
-                        }
-                        else {
-                            getUnreadSMS();
-                        }
-                    }
-                    else if(words.contains(SEND_MY_SMS)) {
-                        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-                            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
-                                Log.i("permission", "should show request permission rationale");
-                            } else {
-                                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSION_SEND_SMS);
-                            }
-                        }
-                        else {
-                            speakOut("To whom");
-                        }                        
-                    }
-                    else if(words.contains("contact")) {
-                        phoneNo = words.replace("contact ", "");
-                        speakOut("Say Message Body");
-                    }
-                    else if(words.contains("type")) {
-                        msgContent = words.replace("type", "");
-                        sendSMS();
-                    }
-                    else {
-                        speakOut("Command not recognized. Please try again.");
-                    }
-                }
-                else {
-                    speakOut("Please try again.");
-                }
+            case MY_PERMISSION_READ_SMS: {
+                getUnreadSMS();
+            }
+            case MY_PERMISSION_SEND_SMS: {
+                speakOut("To whom");
+            }
+            case MY_PERMISSION_RECORD_AUDIO: {
+                promptSpeechInput();
             }
         }
     }
 
+    @Override
+    public void onInit(int status) {
+        final Handler mHandler = new Handler(getApplicationContext().getMainLooper());
+        if (status == TextToSpeech.SUCCESS) {
+            int result = tts.setLanguage(Locale.US);
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+
+            }
+            tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+                @Override
+                public void onStart(String utteranceId) {
+
+                }
+
+                @Override
+                public void onDone(String utteranceId) {
+                    Runnable mDoneRunnable = new Runnable() {
+                        @Override
+                        public void run() {
+                            mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+                        }
+                    };
+                    mHandler.post(mDoneRunnable);
+                    /*mHandler.postAtTime(mDoneRunnable, System.currentTimeMillis()+500);
+                    mHandler.postDelayed(mDoneRunnable, 500);*/
+                }
+
+                @Override
+                public void onError(String utteranceId) {
+                    Log.i("msg", "speech error");
+                }
+            });
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+    }
+
     private void sendSMS() {
-        Log.i("msg", "To = " + phoneNo);
-        Log.i("msg", "Body = " + msgContent);
         Intent smsIntent = new Intent(getApplicationContext(), MainActivity.class);
         PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0, smsIntent, 0);
         SmsManager smsManager = SmsManager.getDefault();
@@ -180,71 +185,6 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         return sb.toString();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case MY_PERMISSION_READ_SMS: {
-                getUnreadSMS();
-            }
-            case MY_PERMISSION_SEND_SMS: {
-                speakOut("To whom");
-            }
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        if (tts != null) {
-            tts.stop();
-            tts.shutdown();
-        }
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        isActivityVisible = false;
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        isActivityVisible = true;
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    public void onInit(int status) {
-        if (status == TextToSpeech.SUCCESS) {
-            int result = tts.setLanguage(Locale.US);
-            if (result == TextToSpeech.LANG_MISSING_DATA
-                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("TTS", "This Language is not supported");
-            } else {
-
-            }
-            tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
-                @Override
-                public void onStart(String utteranceId) {
-                }
-
-                @Override
-                public void onDone(String utteranceId) {
-                    promptSpeechInput();
-                }
-
-                @Override
-                public void onError(String utteranceId) {
-                    Log.i("msg", "speech error");
-                }
-            });
-        } else {
-            Log.e("TTS", "Initilization Failed!");
-        }
-    }
-
     private void speakOut(String textToSpeak) {
         HashMap<String, String> map = new HashMap<String, String>();
         map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "UniqueID");
@@ -271,14 +211,12 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
                 return;
             }
             lastUpdate = actualTime;
-            Log.i("msg", "Device Shaked");
-            if(!isActivityVisible) {
-                Log.i("msg", "Activity Started");
+            Log.i("msg1", "Device Shaked");
+            if(!isOpen) {
+                Log.i("msg1", "Activity Started");
                 Intent mainActivityIntent = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(mainActivityIntent);
-            }
-            else {
-                Log.i("msg", "Activity not visible");
+                isOpen = true;
             }
         }
     }
@@ -287,4 +225,131 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
+    private class SpeechRecognizerListener implements RecognitionListener {
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            Log.i("msg", "ready for speech");
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            Log.i("msg", "speech began");
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            Log.i("msg", "speech end");
+            mSpeechRecognizer.stopListening();
+        }
+
+        @Override
+        public void onError(int error) {
+            Log.i("msg", "speech error" + error);
+            mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            if(results != null) {
+                ArrayList<String> result = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                String words = result.get(0);
+                speechText.setText(result.get(0));
+                if(words.contains(GO_TO_MAP)) {
+                    String extractedText = words.substring(words.indexOf("go to ") + 6);
+                    String[] splited = extractedText.split("\\s+");
+                    String address = "";
+                    for(int i=0; i<splited.length; i++) {
+                        address += splited[i] + "+";
+                    }
+                    Uri gmmIntentUri = Uri.parse("google.navigation:q=" + address);
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    startActivity(mapIntent);
+                }
+                else if(words.contains(READ_SMS)) {
+                    if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_SMS)) {
+                            Log.i("permission", "should show request permission rationale");
+                        } else {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, MY_PERMISSION_READ_SMS);
+                        }
+                    }
+                    else {
+                        getUnreadSMS();
+                    }
+                }
+                else if(words.contains(SEND_MY_SMS)) {
+                    if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.SEND_SMS)) {
+                            Log.i("permission", "should show request permission rationale");
+                        } else {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSION_SEND_SMS);
+                        }
+                    }
+                    else {
+                        speakOut("To whom");
+                    }
+                }
+                else if(words.contains("contact")) {
+                    phoneNo = words.replace("contact ", "");
+                    speakOut("Say Message Body");
+                }
+                else if(words.contains("type")) {
+                    msgContent = words.replace("type", "");
+                    sendSMS();
+                }
+                else {
+                    speakOut("Command not recognized. Please try again.");
+                }
+            }
+            else {
+                speakOut("Please try again.");
+            }
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        mSpeechRecognizer.destroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        isOpen = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isOpen = true;
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
 }
